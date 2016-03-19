255ac8a6bd2fa06d1c93711c99a443a7d6f99f2e
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2016-03-19 05:40:01 +0000

    New worker contexts inaccessible, fixed

--- ManagedObject/Stack.swift
+++ ManagedObject/Stack.swift
@@ -70,7 +70,7 @@ public struct Stack {
   ///   can be used to make changes that asynchronous workers can subsequently
   ///   either save to pass the changes back to the main context, or discard
   ///   without changing the main context.
-  mutating func newWorkerContext() -> NSManagedObjectContext {
+  public mutating func newWorkerContext() -> NSManagedObjectContext {
     let context = NSManagedObjectContext(concurrencyType: .PrivateQueueConcurrencyType)
     context.parentContext = self.mainContext
     return context
